2a11bb9a88d0d8a59c3702212960cfcdd97bc25f
jefBinomed <jean.francois.garreau@gmail.com>
2016-10-03 22:29:54 +0200

    clean class

--- src/scripts/firebase/firebaseAuth.js
+++ src/scripts/firebase/firebaseAuth.js
@@ -7,8 +7,6 @@ export class FireBaseAuth{
             'callbacks': {
                 // Called when the user has been successfully signed in.
                 'signInSuccess': function(user, credential, redirectUrl) {
-                    console.info(user);
-                //handleSignedInUser(user);
                     // Do not redirect.
                     return false;
                 }
@@ -16,20 +14,10 @@ export class FireBaseAuth{
             // Opens IDP Providers sign-in flow in a popup.
             'signInFlow': 'popup',
             'signInOptions': [
-                // TODO(developer): Remove the providers you don't need for your app.
                 {
                 provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
                 scopes: ['https://www.googleapis.com/auth/plus.login']
-                }/*,
-                {
-                provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                scopes :[
-                    'public_profile',
-                    'email',
-                    'user_likes',
-                    'user_friends'
-                ]
-                }*/,
+                },
                 firebase.auth.TwitterAuthProvider.PROVIDER_ID,
                 firebase.auth.GithubAuthProvider.PROVIDER_ID,
                 firebase.auth.EmailAuthProvider.PROVIDER_ID
@@ -87,14 +75,7 @@ export class FireBaseAuth{
         if(this.cbAuthChanged){
             this.cbAuthChanged(user);
         }
-        /*if (user) {
-            user.getToken().then(function(accessToken) {
-                console.info(accessToken);
-            });
-        } else {
-            // User is signed out.
-            console.info('signed out');            
-        }*/
+      
     }
 
     onAuthStateChanged(cb){
